064f69edac1644cf48e5ba573265b3eb5454db05
kunx-edu <kunx-edu@qq.com>
2016-05-08 11:01:51 +0800

    项目的初始化架构

--- admin.shop.com/index.php
+++ admin.shop.com/index.php
@@ -16,11 +16,12 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
-
+define('SITE_ROOT',__DIR__);
+define('RUNTIME_PATH',SITE_ROOT . '/Runtime/');
 // 定义应用目录
-define('APP_PATH','./Application/');
+define('APP_PATH',SITE_ROOT . '/Application/');
 
 // 引入ThinkPHP入口文件
-require './ThinkPHP/ThinkPHP.php';
+require dirname(SITE_ROOT) . '/ThinkPHP/ThinkPHP.php';
 
 // 亲^_^ 后面不需要任何代码了 就是如此简单

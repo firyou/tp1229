f49f4779187cb0670a2f1e67655ce6e185cb8f51
kunx-edu <kunx-edu@qq.com>
2016-05-15 14:17:14 +0800

    权限的编辑和删除

--- admin.shop.com/Application/Admin/Controller/PermissionController.class.php
+++ admin.shop.com/Application/Admin/Controller/PermissionController.class.php
@@ -45,20 +45,48 @@ class PermissionController extends \Think\Controller {
             }
             $this->success('添加成功', U('index'));
         } else {
-            //读取权限列表
-            $permissions = $this->_model->getList();
-            array_unshift($permissions, ['id' => 0, 'name' => '顶级权限', 'parent_id' => null]);
-            $this->assign('permissions', json_encode($permissions));
+            $this->_before_view();
             $this->display();
         }
     }
 
+    /**
+     * 编辑权限.
+     * @param type $id
+     */
     public function edit($id) {
-        $this->display('add');
+        if(IS_POST){
+            if ($this->_model->create() === false) {
+                $this->error($this->_model->getError());
+            }
+            if ($this->_model->savePermission() === false) {
+                $this->error($this->_model->getError());
+            }
+            $this->success('修改成功', U('index'));
+        }else{
+            //获取一条记录
+            $this->assign('row', $this->_model->find($id));
+            $this->_before_view();
+            $this->display('add');
+        }
     }
 
+    /**
+     * 删除权限
+     * @param integer $id
+     */
     public function delete($id) {
-        
+        if($this->_model->deletePermission($id) === false){
+            $this->error($this->_model->getError());
+        }
+        $this->success('删除成功', U('index'));
+    }
+
+    private function _before_view() {
+        //读取权限列表
+        $permissions = $this->_model->getList();
+        array_unshift($permissions, ['id' => 0, 'name' => '顶级权限', 'parent_id' => null]);
+        $this->assign('permissions', json_encode($permissions));
     }
 
 }
--- admin.shop.com/Application/Admin/Model/PermissionModel.class.php
+++ admin.shop.com/Application/Admin/Model/PermissionModel.class.php
@@ -13,32 +13,71 @@ namespace Admin\Model;
  *
  * @author qingf
  */
-class PermissionModel extends \Think\Model{
+class PermissionModel extends \Think\Model {
+
     protected $_validata = [
-        ['name','require','权限名称不能为空',self::EXISTS_VALIDATE,'',self::MODEL_BOTH],
-        ['parent_id','require','父级权限不能为空',self::EXISTS_VALIDATE,'',self::MODEL_BOTH],
+        ['name', 'require', '权限名称不能为空', self::EXISTS_VALIDATE, '', self::MODEL_BOTH],
+        ['parent_id', 'require', '父级权限不能为空', self::EXISTS_VALIDATE, '', self::MODEL_BOTH],
     ];
-    
-    
+
     /**
      * 获取分类列表
      */
-    public function getList(){
-        return $this->where(['status'=>1])->order('lft')->select();
+    public function getList() {
+        return $this->where(['status' => 1])->order('lft')->select();
     }
-    
+
     /**
      * 使用nestedsets添加权限.
      */
     public function addPermission() {
         unset($this->data[$this->getPk()]);
         //创建orm
-        $orm = D('NestedSetsMysql','Logic');
+        $orm        = D('NestedSetsMysql', 'Logic');
         $nesetdsets = new \Admin\Service\NestedSets($orm, $this->trueTableName, 'lft', 'rght', 'parent_id', 'id', 'level');
-        if($nesetdsets->insert($this->data['parent_id'], $this->data, 'bottom') === false){
+        if ($nesetdsets->insert($this->data['parent_id'], $this->data, 'bottom') === false) {
             $this->error = '添加失败';
             return false;
         }
         return true;
     }
+
+    /**
+     * 编辑权限.
+     * @return boolean
+     */
+    public function savePermission() {
+        $request_data = $this->data;
+        //1.判断是否修改了父级权限
+        $parent_id    = $this->getFieldById($request_data['id'], 'parent_id');
+        //1.1如果修改了,使用nestedsets计算新的左右节点和层级
+        if ($parent_id != $request_data['parent_id']) {
+            //创建orm
+            $orm        = D('NestedSetsMysql', 'Logic');
+            $nesetdsets = new \Admin\Service\NestedSets($orm, $this->trueTableName, 'lft', 'rght', 'parent_id', 'id', 'level');
+            if ($nesetdsets->moveUnder($request_data['id'], $request_data['parent_id'], 'bottom') === false) {
+                $this->error = '父级分类不合法';
+                return false;
+            }
+        }
+        //1.2如果没有修改,直接保存
+        return $this->save();
+    }
+
+    /**
+     * 删除权限.使用物理删除
+     * @param type $id
+     * @return boolean
+     */
+    public function deletePermission($id) {
+        $orm        = D('NestedSetsMysql', 'Logic');
+        $nesetdsets = new \Admin\Service\NestedSets($orm, $this->trueTableName, 'lft', 'rght', 'parent_id', 'id', 'level');
+        if ($nesetdsets->delete($id) === false) {
+            $this->error = '删除失败';
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }

c9a8a7f1a2307d401de99108159ccd0dd0039903
kunx-edu <kunx-edu@qq.com>
2016-05-18 15:59:59 +0800

    根据权限看菜单

--- admin.shop.com/Application/Admin/Model/MenuModel.class.php
+++ admin.shop.com/Application/Admin/Model/MenuModel.class.php
@@ -150,7 +150,7 @@ class MenuModel extends \Think\Model {
         //获取当前用户的权限列表
         $pids = permission_ids();
         //获取所有的可访问菜单.
-        return $this->field('id,name,path,parent_id')->distinct(true)->alias('m')->join('left join __MENU_PERMISSION__ as mp ON m.`id`=mp.`menu_id`')->select();
+        return $this->field('id,name,path,parent_id')->distinct(true)->alias('m')->join('left join __MENU_PERMISSION__ as mp ON m.`id`=mp.`menu_id`')->where(['permission_id' => ['in', $pids]])->select();
     }
 
 }

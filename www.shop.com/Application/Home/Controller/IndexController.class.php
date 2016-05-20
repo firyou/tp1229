7fc6136d303f4f106c00f32c9ccde2d123ce9c5f
kunx-edu <kunx-edu@qq.com>
2016-05-20 11:53:06 +0800

    数据缓存

--- www.shop.com/Application/Home/Controller/IndexController.class.php
+++ www.shop.com/Application/Home/Controller/IndexController.class.php
@@ -6,25 +6,51 @@ use Think\Controller;
 
 class IndexController extends Controller {
 
-    protected function _initialize(){
-        //取出分类
-        $goods_categories = M('GoodsCategory')->where(['status' => 1])->select();
-        //传入视图
-        $this->assign('goods_categories', $goods_categories);
+    protected function _initialize() {
         //首页才展示分类列表.
-        if(ACTION_NAME=='index'){
+        if (ACTION_NAME == 'index') {
             $is_show_cat = true;
-        }else{
+        } else {
             $is_show_cat = false;
         }
         $this->assign('is_show_cat', $is_show_cat);
-    }
-    public function index() {
+
+        //由于文章分类和帮助文章并不会经常更新,所以我们可以使用数据缓存 标识是ART_CATS
+        $goods_categories = S('GOODS_CAT');
+        if (!$goods_categories) {
+            //取出分类
+            $goods_categories = M('GoodsCategory')->where(['status' => 1])->select();
+            S('GOODS_CAT', $goods_categories, 300);
+        }
+
+
+        $article_categories = S('ART_CATS');
+        if (!$article_categories) {
+            //获取帮助文章分类
+            $article_categories = M('ArticleCategory')->where(['is_help' => 1, 'status' => 1])->getField('id,name');
+            S('ART_CATS', $article_categories, 300);
+        }
+        //获取各分类的文章
+        $artilce_list = S('ART_LIST');
+        if (!$artilce_list) {
+            foreach ($article_categories as $article_cat_id => $article_cat) {
+                $artilce_list[$article_cat_id] = M('Article')->where(['id' => $article_cat_id])->limit(6)->getField('id,id,name');
+            }
+            S('ART_LIST', $artilce_list, 300);
+        }
         
+        //传入视图
+        $this->assign('goods_categories', $goods_categories);
+        $this->assign('article_categories', $article_categories);
+        $this->assign('article_list', $artilce_list);
+    }
+
+    public function index() {
+
         $this->display();
     }
-    
-    public function goods($id){
+
+    public function goods($id) {
         $this->display();
     }
 
